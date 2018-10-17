df0bdd2824c9b809f9110dc1d734d2615d5aa93f
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2018-10-17 14:31:44 +0300

    Added project name resolving

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-// const { EventEmitter } = require('events');
+const fs = require('fs');
+const path = require('path');
 const gitStatus = require('./libs/gitStatus');
 const deployStack = require('./libs/deployStack');
 
-
 module.exports = (args) => {
-  let branchname;
+  let branchname = '';
+
+  // Resolve project name
+  let projectname = '';
+  if (fs.existsSync('./package.json')) {
+    const stringPackageJson = fs.readFileSync('./package.json', { encoding: 'utf8' });
+    try {
+      const packageJson = JSON.parse(stringPackageJson);
+      projectname = packageJson.name;
+    } catch (err) {
+      throw err;
+    }
+  } else {
+    projectname = path.basename(process.cwd());
+  }
+
+  // Make sure we have everything
+  if (!projectname) {
+    throw new Error('Could not resolve project name');
+  }
+
+  const stackname = `${projectname}-${branchname}`;
 
   new Promise(resolve => resolve())
     .then(() => gitStatus())
     .then((newBranchname) => { branchname = newBranchname; })
-    .then(() => deployStack(branchname, args.stack, args.params))
-    .catch(err => console.error('error:', err.message));
+    .then(() => deployStack(stackname, args))
+    .catch(err => console.error('Error:', err.message));
 };
